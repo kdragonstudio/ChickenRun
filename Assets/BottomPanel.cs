de9b17fc9da6c9ad599217141e68d4fc59ba6521
kdragonstudio <kdragon.kyungtae@gmail.com>
2015-02-12 02:55:56 +0900

    It is first commit to new project
    it is test commit to new project with GitHub

---
+++ Assets/BottomPanel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BottomPanel : MonoBehaviour 
+{
+	public Text title;
+	// Use this for initialization
+	void Start () 
+	{
+		title.text = "Hellow Chicken Run";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
